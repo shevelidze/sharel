cfdd966feb7767250bfb7e9873f9ad5796eaa7cf
Denys Shevel <denys.shevel16@gmail.com>
2021-12-17 23:20:12 +0200

    Distribute css

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,30 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log(styles);
   return (
-    <div id="root">
-      <div id="main-content">
-        <div id="tagline">
+    <div id={styles.root}>
+      <div id={styles.main_content}>
+        <div id={styles.tagline}>
           <div>
             <p>Share your</p>
             <p>thoughts</p>
             <p>with people</p>
-            <p>using <span className="logo">sharel</span></p>
-            <div id="buttons-wrapper">
-              <div>Sign up</div>
-              <div>Sign in</div>
+            <p>using <span className={styles.logo}>sharel</span></p>
+            <div id={styles.buttons_wrapper}>
+              <a href="/sign_up">Sign up</a>
+              <a href="/sign_in">Sign in</a>
             </div>
           </div>
         </div>
-        <div id="illustrations">
-          <img className="textbox" src="/main-page/box1.png" />
-          <img className="textbox" src="/main-page/box2.png" />
-          <img className="textbox" src="/main-page/box3.png" />
+        <div id={styles.illustrations}>
+          <img className={styles.textbox} src="/main-page/box1.png" />
+          <img className={styles.textbox} src="/main-page/box2.png" />
+          <img className={styles.textbox} src="/main-page/box3.png" />
         </div>
       </div>
-      <div id="footer">
-        <div id="soocial">
+      <div id={styles.footer}>
+        <div id={styles.social}>
           <i className="fab fa-telegram-plane"></i>
         </div>
       </div>
