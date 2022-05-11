43d206381beb056edb2642a3e0b38c748a7cee90
Denys Shevel <denys.shevel16@gmail.com>
2022-05-11 15:05:44 -0400

    Set up tokens creating

--- components/signIn.js
+++ components/signIn.js
@@ -1,23 +1,56 @@
-import Input from "../components/Input"
-import styles from '../styles/Auth.module.css'
-import Button from "../components/Button"
+import { useRef } from 'react';
+import Input from '../components/Input';
+import styles from '../styles/Auth.module.css';
+import Button from '../components/Button';
+import { apiFetch } from '../lib/apiFetch';
+
+async function signIn(emailInputRef, passwordInputRef) {
+    let response = await apiFetch(
+        '/sign_in',
+        {
+            password: passwordInputRef.current.value,
+            email: emailInputRef.current.value,
+        },
+        false
+    );
+    console.log(response);
+}
 
 export default function SignIn(props) {
+    let emailInputRef = useRef();
+    let passwordInputRef = useRef();
     return (
         <div id={styles.root}>
             <div id={styles['title-wrapper']}>
                 <div>
-                    <svg onClick={props.onBackClick} width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" fill="white" clipRule="evenodd"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" /></svg>
+                    <svg
+                        onClick={props.onBackClick}
+                        width="24"
+                        height="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fillRule="evenodd"
+                        fill="white"
+                        clipRule="evenodd"
+                    >
+                        <path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" />
+                    </svg>
                     <div>Sign in</div>
                 </div>
             </div>
             <div id={styles['main-block']}>
                 <div className={styles['section']}>
-                    <Input placeholder='Email'></Input>
-                    <Input placeholder='Password' type='password'></Input>
+                    <Input placeholder="Email" ref={emailInputRef}></Input>
+                    <Input
+                        placeholder="Password"
+                        type="password"
+                        ref={passwordInputRef}
+                    ></Input>
                 </div>
-                <Button text='Sign in'></Button>
+                <Button
+                    text="Sign in"
+                    onClick={signIn.bind(null, emailInputRef, passwordInputRef)}
+                ></Button>
             </div>
         </div>
-    )
+    );
 }
--- pages/api/sign_in.js
+++ pages/api/sign_in.js
@@ -1,28 +1,45 @@
-import prisma from '../../lib/prisma'
-import validate from '../../lib/validator'
-import crypto from 'crypto'
+import prisma from '../../lib/prisma';
+import validateData from '../../lib/requestDataValidator';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
 
-export default validate(
+export default validateData(
     async (request, response) => {
-        let passwordHash = crypto.createHash('sha256').update(request.body.password).digest('base64');
+        let passwordHash = crypto
+            .createHash('sha256')
+            .update(request.body.password)
+            .digest('base64');
         let userData = await prisma.users.findFirst({
             where: {
                 password_hash: passwordHash,
-                email: request.body.email
-            }
+                email: request.body.email,
+            },
         });
-        console.log(userData);
-        response.json(userData);
+        if (userData) {
+            response.json({
+                JWTAccessToken: jwt.sign(
+                    {
+                        userId: userData.id,
+                    },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '30d' }
+                ),
+            });
+        } else {
+            response.status(400).json({
+                message: 'Wrong email or password.',
+                errors: ['wrongUserData'],
+            });
+        }
     },
-    'POST',
     {
         properties: {
             password: {
-                type: 'string'
+                type: 'string',
             },
             email: {
-                type: 'string'
-            }
-        }
+                type: 'string',
+            },
+        },
     }
 );
