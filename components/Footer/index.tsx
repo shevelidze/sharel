954570cc9363219ca343731115e84599fdc59edb
Denys Shevel <denys.shevel16@gmail.com>
2022-07-11 09:10:35 -0400

    Add landing page

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,10 +6,11 @@ import {
 } from './socialIcons';
 import React from 'react';
 import styles from './Footer.module.css';
+import landingSectionStyles from '../../styles/LandingSection.module.css';
 
 const Footer: React.FC = () => {
   return (
-    <div className={styles.root}>
+    <div className={[styles.root, landingSectionStyles.section].join(' ')}>
       <div className={styles.content}>
         <div>
           <div>© 2022 sharel</div>
--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -2,14 +2,30 @@ import React from "react";
 import Image from "next/image";
 import interfaceDemoImage from './interfaceDemo.png';
 import { LinkButton } from '../Buttons';
+import landingSectionStyles from '../../styles/LandingSection.module.css';
+import styles from './Landing.module.css';
+import useUser from '../../lib/useUser';
 
 const Landing: React.FC = () => {
+  const [user] = useUser();
+
   return (
-    <div>
-      <Image src={interfaceDemoImage} alt="Interface demo." />
-      <div>
-        <LinkButton href="/sign_in">Sign in</LinkButton>
-        <LinkButton href="/sign_up">Sign up</LinkButton>
+    <div className={landingSectionStyles.section}>
+      <div className={styles.header}>
+        <h1>sharel</h1>
+        <div className={styles.buttonsWrapper}>
+          {user === null ? (
+            <>
+              <LinkButton href="/sign_in">Sign in</LinkButton>
+              <LinkButton href="/sign_up">Sign up</LinkButton>
+            </>
+          ) : (
+            <LinkButton href="/home">To posts</LinkButton>
+          )}
+        </div>
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image src={interfaceDemoImage} alt="Interface demo." />
       </div>
     </div>
   );
